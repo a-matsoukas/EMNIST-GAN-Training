e59cdfa8a532abe17062786b2705d0af59802c7a
a-matsoukas <78324723+a-matsoukas@users.noreply.github.com>
2021-12-12 14:39:07 -0500

    Update activation_functions.py
    Add leaky relu function and its derivative.

--- activation_functions.py
+++ activation_functions.py
@@ -2,15 +2,14 @@
 This module contains helper functions for performing forward passes through the
 GAN and for computing the gradient prior to backpropagation.
 
-Specifically, this module contains the ReLU activation function, the Sigmoid
-activation function, and their derivatives.
+Specifically, this module contains the ReLU activation function, the Leaky ReLU
+activation function, the Sigmoid activation function, and their derivatives.
 """
 import numpy as np
 
 def relu(node_value_array):
     """
-    Perform the ReLU function on a given array. This will be used as the
-    activation function for the hidden layers of the MLPs.
+    Perform the ReLU function on a given array. 
 
     Args:
         node_value_array: A numpy array containing the values of the nodes at a
@@ -22,8 +21,7 @@ def relu(node_value_array):
 
 def sigmoid(node_value_array):
     """
-    Perform the Sigmoid function on a given array. This will be used as the
-    activation function for the final layer of the discriminator and generator.
+    Perform the Sigmoid function on a given array. 
 
     Args:
         node_value_array: A numpy array containing the values of the nodes at a
@@ -32,6 +30,19 @@ def sigmoid(node_value_array):
         The values in the array after passing it through Sigmoid. 
     """
     return 1 / (1 + np.exp(- node_value_array))
+
+def leaky_relu(node_value_array):
+    """
+    Perform the Leaky RelU function on a given array.
+
+    Args: 
+        node_value_array: A numpy array containing the values of the nodes at a
+            given layer, prior to activation.
+    Returns:
+        The values in the array after passing it through Leaky ReLU. 
+    """
+    alpha = .1
+    return (alpha * (node_value_array < 0) + 1 * (node_value_array >= 0)) * node_value_array
 
 def sigmoid_prime(node_value_array):
     """
@@ -58,3 +69,17 @@ def relu_prime(node_value_array):
         The values in the array after passing it through ReLU'.    
     """
     return (node_value_array > 0) * 1
+
+def leaky_relu_prime(node_value_array):
+    """
+    Perform the derivative of the Leaky ReLU function on a given array. This
+    will be used for backpropagation.
+
+    Args:
+        node_value_array: A numpy array containing the values of the nodes at a
+            given layer.
+    Returns:
+        The values in the array after passing it through (Leaky ReLU)'.
+    """
+    alpha = .1
+    return alpha * (node_value_array < 0) + 1 * (node_value_array >= 0)
